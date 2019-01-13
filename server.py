d9eaeb91a25aa08ee1d66bc4d277a9ffcf6df73a
Sunny Farm <yummy@yummy.home>
2019-01-13 08:13:24 -0500

    tune

--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
-from flask import Flask
+from flask import Flask, Response, request, abort
 from functools import wraps
-from flask import request, abort
 import ssl
+import json
+import cv2
+import numpy as np
+import jsonpickle
+import base64
+from werkzeug import secure_filename
+
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('server.crt', 'server.key')
 
@@ -23,7 +29,6 @@ def require_appkey(view_function):
             key=apikey.readline().replace('\n', '')
             
             while key:
-                print(key)
                 if input_key == key:
                     return view_function(*args, **kwargs)
                 key = apikey.readline().replace('\n', '')
@@ -36,7 +41,27 @@ def require_appkey(view_function):
 @app.route('/api/v1/query', methods=['POST'])
 @require_appkey
 def query():
-    return 'Good\n'
+    json_str = request.json
+    json_out = json.loads(json_str)
+    if json_out['image'] is not None:
+        image = json_out['image']
+        data = base64.b64decode(image)
+        try:
+            f = open('out.jpg', 'wb') 
+            f.write(data)
+        finally:
+            f.close()
+    #print(request.json, request.data, request.files)
+    
+    #image = f['files']
+    #json = f['json']
+    
+    # decode image
+    response = {'message': 'image received'}
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
 
 @app.route('/')
 def index_page():
